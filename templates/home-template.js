124b85b37a3f11e1e530b6a28953a79defe053ab
Leonel Oliveira - OnR <loliveira@hq.onresearch.com>
2019-03-06 12:23:12 -0500

    layout's updates

--- templates/home-template.js
+++ templates/home-template.js
@@ -4,14 +4,22 @@ const HomeTemplate = `
   	<div class="main-content">
   		<div class="item">
 				<img src="image/book01.png" />
+				<button type="button" class="btn-add">Add to Cart</button>
   		</div>
   		<div class="item">
 				<img src="image/book02.png" />
+				<button type="button" class="btn-add">Add to Cart</button>
   		</div>
   		<div class="item">
 				<img src="image/book03.png" />
+				<button type="button" class="btn-add">Add to Cart</button>
+  		</div>
+  		<div class="item">
+				<img src="image/book04.png" />
+				<button type="button" class="btn-add">Add to Cart</button>
   		</div>
   	</div>
+  	
   </div>
 `
 export { HomeTemplate }
--- templates/main-template.js
+++ templates/main-template.js
@@ -2,6 +2,9 @@ const MainTemplate = `
   <div>
   	<navbar></navbar>
   	<div class="center info-bar">Learning Vuejs Single Page Application</div>
+  	<div class="banner-container">
+  		<img src="image/banner.jpg" />
+  	</div>
   	<div class="content container">
     	<router-view></router-view>
   	</div>
